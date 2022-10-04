2bdb512df6bec8a869180d5d51cc8923d27746be
Liam Lande <liam.lande@gmail.com>
2022-10-04 16:21:54 +0200

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Configuration;
+using Contentful.AspNetCore;
+
+
+namespace Contenful_Test
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/404");
+                app.UseHsts();
+            }
+
+            app.UseCookiePolicy();
+            
+
+            
+
+            app.UseHttpsRedirection();
+
+            
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+
+
+        }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddHttpClient();
+
+            services.AddRazorPages();
+
+
+            services.AddContentful(Configuration);
+
+
+        }
+
+    }
+}
